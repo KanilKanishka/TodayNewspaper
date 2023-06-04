5848d0a1753aca0aad65b252823fd6d2cd547d0f
Kanilkanishka <kanilkanishka@gmail.com>
2023-06-04 23:18:49 +0530

    Crete login

--- app/src/main/java/com/example/todaynewspaper/MainActivity.java
+++ app/src/main/java/com/example/todaynewspaper/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.todaynewspaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText etUsername, etPassword;
+    private Button btnLogin;
+
+    private String validUsername = "admin";
+    private String validPassword = "password";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+            etUsername = findViewById(R.id.username);
+            etPassword = findViewById(R.id.password);
+            btnLogin = findViewById(R.id.loginButton);
+
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String username = etUsername.getText().toString();
+                    String password = etPassword.getText().toString();
+
+                    if (username.equals(validUsername) && password.equals(validPassword)) {
+                        // Login successful
+                        Toast.makeText(MainActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                        // Start the main activity or any other desired action
+                        Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        // Login failed
+                        Toast.makeText(MainActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
     }
-}
+
