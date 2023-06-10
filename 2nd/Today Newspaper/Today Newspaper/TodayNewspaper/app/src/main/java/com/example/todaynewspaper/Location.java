d3c55d7490f74a347675572e29be7ea285a008ab
Kanilkanishka <kanilkanishka@gmail.com>
2023-06-10 14:47:39 +0530

    Resolved map location issue

---
+++ 2nd/Today Newspaper/Today Newspaper/TodayNewspaper/app/src/main/java/com/example/todaynewspaper/Location.java
@@ -0,0 +1,134 @@
+package com.example.todaynewspaper;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class Location extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng defaultLocation = new LatLng(6.929146, 79.863926);
+        float zoomLevel = 13.0f;
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(defaultLocation)
+                .zoom(zoomLevel)
+                .build();
+        mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
+        // Check for location permission
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Request location permission
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    1);
+        }
+
+        new FetchJSONTask().execute("http://10.0.2.2/subscribers.json");
+    }
+
+    private class FetchJSONTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String url = urls[0];
+            String result = "";
+
+            try {
+                URL urlObject = new URL(url);
+                HttpURLConnection connection = (HttpURLConnection) urlObject.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+                result = stringBuilder.toString();
+                reader.close();
+                inputStream.close();
+                connection.disconnect();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            try {
+                JSONArray jsonArray = new JSONArray(result);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    double latitude = jsonObject.getDouble("latitude");
+                    double longitude = jsonObject.getDouble("longitude");
+                    String name = jsonObject.getString("name");
+                    LatLng location = new LatLng(latitude, longitude);
+                    mMap.addMarker(new MarkerOptions().position(location).title(name));
+                }
+
+                // Zoom and center the map camera to the first point
+                if (jsonArray.length() > 0) {
+                    JSONObject firstPoint = jsonArray.getJSONObject(0);
+                    double firstLatitude = firstPoint.getDouble("latitude");
+                    double firstLongitude = firstPoint.getDouble("longitude");
+                    LatLng firstLocation = new LatLng(firstLatitude, firstLongitude);
+
+                    CameraPosition cameraPosition = new CameraPosition.Builder()
+                            .target(firstLocation)
+                            .zoom(12)
+                            .build();
+                    mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
